57b99b9334958f64324aea806630aaee52f92d0e
Dhirajrrajput123 <dhirajrajpp@gmail.com>
2022-11-02 12:07:23 +0530

    code will be fixed

--- primeProblem.js
+++ primeProblem.js
@@ -1,12 +1,14 @@
-//prime problem
+//to  find the number is prime number or not 
 let number=19;
-let count=0;
-fot(let i=0;i<=number;i++){
-count++;
+let count_of_divisible=0;
+fot(let i=1;i<=number;i++){
+if(number%i==0){
+count_of_divisible++;
 }
-if(count==2){
-console.log("yes");
+}
+if(count_of_divisible==2){
+console.log(number," is a prime number");
 }
 else{
-console.log("no");
+console.log(number," is not a prime number");
 }
